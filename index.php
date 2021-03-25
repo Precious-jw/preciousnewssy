1a3dd2b4e039eabb3e217a62dbee70c1b07e4ef5
Precious Newssy <you@example.comifakaprecious43@gmail.com>
2021-03-25 14:18:37 +0100

    Minor modifications

--- index.php
+++ index.php
@@ -119,23 +119,30 @@
                 <div class="skills">
                     <div class="skills-row-1">
                         <section class="html-progress progress">
-                            <span class="skill-html skills-flex"><p class="html">Html</p><p class="html-percent">85%</p></span>
+                            <span class="skill-html skill"><p class="html">Html</p><p class="html-percent percent">85%</p></span>
                             <progress value="85" max="100"></progress>
                         </section>
                     
                         <section class="css-progress progress">
-                            <span class="skill-css"><p class="css">CSS</p><p class="css-percent">80%</p></span>
+                            <span class="skill-css skill"><p class="css">CSS</p><p class="css-percent percent">80%</p></span>
                             <progress value="80" max="100"></progress>
                         </section>
-                    </div>
-                    <div class="skills-row-2">
+
                         <section class="js-progress progress">
-                            <span class="skill-js skills-flex"><p class="js">Javascript</p><p class="js-percent">60%</p></span>
+                            <span class="skill-js skill"><p class="js">Javascript</p><p class="js-percent percent">60%</p></span>
                             <progress value="60" max="100"></progress>
                         </section>
+                        
+                    </div>
+                    <div class="skills-row-2">
                         <section class="php-progress progress">
-                            <span class="skill-php"><p class="php">PHP</p><p class="php-percent">60%</p></span>
+                            <span class="skill-php skill"><p class="php">PHP</p><p class="php-percent percent">60%</p></span>
                             <progress value="60" max="100"></progress>
+                        </section>
+
+                        <section class="sql-progress progress">
+                            <span class="skill-sql skill"><p class="sql">SQL</p><p class="sql-percent percent">80%</p></span>
+                            <progress value="80" max="100"></progress>
                         </section>
                     </div>
                 </div>
@@ -188,7 +195,7 @@
                     <span class="services-row-1 services-rows">
                         <img src="images/responsive-web.jpg">
                         <span class="right-content">
-                                <strong>RESPONSIVE WEB DESIGN</strong>
+                                <strong>RESPONSIVE WEBSITE DEVELOPMENT</strong>
                             <p class="text-body">
                                 I can create a website that will be <strong>Responsive</strong> on all type of devices.
                             </p>
@@ -197,22 +204,22 @@
 
                     <span class="services-row-2 services-rows">
                         <span class="right-content">
-                                <strong>Blog</strong>
+                                <strong>ONLINE BLOG</strong>
                             <p class="text-body">
                                 I can help you set up an <strong>Online Blog</strong> with minimal cost.
                             </p>
                         </span>
 
-                        <img src="images/perfect web1.png">
+                        <img src="images/blog.jpg">
                     </span>
 
                     <span class="services-row-3 services-rows">
-                        <img src="images/portfolio.JPG">
+                        <img src="images/manager.jpg">
 
                         <span class="right-content">
-                                <strong>WEBSITE MODIFICATION</strong>
+                                <strong>WEBSITE MANAGER</strong>
                             <p class="text-body">
-                                If you are looking for a web developer to fix some issues or make some changes to your website, then I may be of help.
+                                I can help you manage various aspects of your website such as updating, regular maintenance and performance optimization.
                             </p>
                         </span>
                     </span>
@@ -228,9 +235,12 @@
 
                 <div class="hire-main">
                     <span class="hire-rows">
-                        <span>FIVERR</span>
-                        <p class="text-body">
-                            Check out and purchase some of my gigs on <br> <a href="www.fiver.com">fiverr.</a>
+                        <span>WHATSAPP</span>
+                        <p class="text-body hire-wa">
+                            Chat me on whatsapp<br>
+                            <a href="https://api.whatsapp.com/send?phone=2347059035326" target="blank" class="wa">
+                                <img src="images/whatsapp logo.png" alt="Whatsapp image" class="wa-img">
+                            </a>
                         </p>
                     </span>
                     
@@ -244,7 +254,7 @@
                     <span class="hire-rows">
                         <span>FACEBOOK</span>
                         <p class="text-body hire-facebook">
-                            You can also send me a message on <a href="https://web.facebook.com/profile.php?id=100018896417862" target="blank" class="fb"><br>
+                            You can also send me a message on Facebook <br> <a href="https://web.facebook.com/profile.php?id=100018896417862" target="blank" class="fb">
                                 <i class="fab fa-facebook-square"></i>
                                 </a>
                         </p>
